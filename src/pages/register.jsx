c7bb9a03506640001c28f4c0a8fa80c3e58062ac
Colin Stewart <colin.mac.stewart1@gmail.com>
2023-01-05 09:48:39 -0800

    Added to headers

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -32,8 +32,15 @@ export default function Register() {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Max-Age': 2592000,
+        'Access-Control-Allow-Credentials': true,
       },
+
       body: JSON.stringify(form)
     })
 
