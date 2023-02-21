981d506069448db29c4fbaca74f510896d5dc89e
Colin Stewart <colin.mac.stewart1@gmail.com>
2023-02-21 13:59:38 -0500

    Added link to login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
   return (
     <>
       <Head>
-        <title>Sign In - RightWork</title>
+        <title>Go To App Login - RightWork</title>
       </Head>
       <AuthLayout>
         <div className="flex flex-col">
@@ -27,7 +27,7 @@ export default function Login() {
           </Link>
           <div className="mt-10">
             <h2 className="text-lg font-semibold text-gray-900">
-              Sign in to your account
+              Go to RightWork app login
             </h2>
             <p className="mt-2 text-sm text-gray-700">
               Don’t have an account?{' '}
@@ -41,36 +41,19 @@ export default function Login() {
             </p>
           </div>
         </div>
-        <form action="#" className="mt-10 grid grid-cols-1 gap-y-8">
-          <TextField
-            label="Email address"
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-          />
-          <TextField
-            label="Password"
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-          />
-          <div>
+        <div className="mt-10">
+          <a href="https://getrightwork.dev/">
             <Button
-              type="submit"
               variant="solid"
               color="blue"
-              className="w-full"
+              className="w-3/4"
             >
               <span>
-                Sign in <span aria-hidden="true">&rarr;</span>
+                Go to RightWork app login <span aria-hidden="true">&rarr;</span>
               </span>
             </Button>
-          </div>
-        </form>
+          </a>
+        </div>
       </AuthLayout>
     </>
   )
